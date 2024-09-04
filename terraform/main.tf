1c08f7bc8d4caaa2d03fc08bde2b03a10ee179b8
Karthikeyan21001828 <karthi06062004@gmail.com>
2024-09-04 10:28:58 +0530

    chages made in variables.tf and removing the direct values from main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,10 +4,7 @@ provider "aws" {
 
 module "app_server" {
   source        = "./modules/app_server"
-  vpc_id        = "vpc-024e76b5f8ee97841"
-  ami_id         = "ami-04cdc91e49cb06165"
-  instance_type  = "t3.medium"
-  key_name       = "DevOpsKeyPair"
+  
 }
 
 output "app_server_ip" {
--- terraform/modules/app_server/variables.tf
+++ terraform/modules/app_server/variables.tf
@@ -1,20 +1,23 @@
 variable "vpc_id" {
   description = "The VPC ID where the instance will be deployed."
+  default     = "vpc-024e76b5f8ee97841"
   type        = string
 }
 
 variable "ami_id" {
   description = "The AMI ID to use for the instance."
+  default     = "ami-04cdc91e49cb06165"
   type        = string
 }
 
 variable "instance_type" {
   description = "The type of the instance."
-  default     = "t2.micro"
+  default     = "t3.medium"
   type        = string
 }
 
 variable "key_name" {
   description = "The key name for SSH access."
+  default     = "DevOpsKeyPair"
   type        = string
 }
